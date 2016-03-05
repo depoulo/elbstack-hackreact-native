45d672f9abb12bb0f986c4f96485f7caaf022495
Paolo Priotto <p.priotto@epages.com>
2016-03-05 14:13:44 +0100

    add redux logger middleware

--- shared/configureStore.js
+++ shared/configureStore.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
+import createLogger from 'redux-logger'
 
 export default function configureStore(initialState) {
   const enhancer = compose(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, createLogger())
   )
 
   return createStore(rootReducer, initialState, enhancer)
